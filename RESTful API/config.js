9276b1ba6b093173a2816e49cb3d21533a44aca7
Manuel de Jesús González Arvelo <manuelarvelo19@gmail.com>
2018-08-28 23:00:52 +0200

    soporte para https agregado

--- RESTful API/config.js
+++ RESTful API/config.js
@@ -1,5 +1,7 @@
 /**
  * Create an export configuration variables.
+ * To generate the files for https server sec: 
+ * openssl req -newkey rsa:2048 -new -nodes -x509 -days -3650 -keyout key.pem -out cert.pem
  */
 
  //Container for all variables.
